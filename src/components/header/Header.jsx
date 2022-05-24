2abb9fc7e01dc5e0e9130d09864b6de5e36e1a5c
Stray <strayfrozen@yahoo.com>
2022-05-24 15:00:28 -0500

    set up header component as well as header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/coon.png'
+import ME from '../../assets/coon.jpg'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
@@ -10,11 +10,11 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Chris Langford</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocial />
 
-        <div className="me">
+      <div className="me">
         <img src={ME} alt="me" />
       </div>
 
