f2504885335b6e40146c6e694550ffe39222ab48
Stray <strayfrozen@yahoo.com>
2022-05-24 13:12:47 -0500

    added downloadable resume and image placeholder to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/coon.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Chris Langford</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+
+        <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+
+      <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://www.linkedin.com/in/chris-langford-662793101/" target="_blank"></a>
+        <a href="https://github.com/strayfrozen" target="_blank"></a>
+        <a href="https://dribbble.com" target="_blank"></a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
