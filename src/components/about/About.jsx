96cca77dd79b8434c1f89b98eab555b6d9074288
Stray <strayfrozen@yahoo.com>
2022-05-25 09:20:49 -0500

    added nav bar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/coon.jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
